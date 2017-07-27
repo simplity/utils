83111514277c5cf49ed5375fa4147a56c3ab3d57
Madhavan Neelakandan <Madhavan_N@ad.infosys.com>
2017-07-27 16:23:22 +0530

    detect hyperlink functionality updated

--- plugin/bundles/org.simplity.eclipse.plugin.validator/src/org/simplity/eclipse/plugin/validator/builder/AddRemoveSampleNatureHandler.java
+++ plugin/bundles/org.simplity.eclipse.plugin.validator/src/org/simplity/eclipse/plugin/validator/builder/AddRemoveSampleNatureHandler.java
@@ -24,6 +24,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.m2e.core.MavenPlugin;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.simplity.kernel.Application;
 import org.simplity.kernel.util.ReflectUtil;
@@ -66,30 +67,41 @@ public class AddRemoveSampleNatureHandler extends AbstractHandler {
 	 *            to have sample nature added or removed
 	 */
 	private void toggleNature(IProject project) throws CoreException {
-		IProjectDescription description = project.getDescription();
-		String[] natures = description.getNatureIds();
+		IProjectDescription projectDescription = project.getDescription();
+		String[] projectNature = projectDescription.getNatureIds();
+		boolean projectHasMavenNature = false;
+	    for(String nature : projectNature) {
+	    	if(nature.contentEquals("org.eclipse.m2e.core.maven2Nature")) {
+	    		projectHasMavenNature = true;
+		    	break;
+	    	}
+	    }
+	    if(projectHasMavenNature) {
+	    	IProjectDescription description = project.getDescription();
+			String[] natures = description.getNatureIds();
 
-		for (int i = 0; i < natures.length; ++i) {
-			if (SampleNature.NATURE_ID.equals(natures[i])) {
-				// Remove the nature
-				String[] newNatures = new String[natures.length - 1];
-				System.arraycopy(natures, 0, newNatures, 0, i);
-				System.arraycopy(natures, i + 1, newNatures, i, natures.length - i - 1);
-				description.setNatureIds(newNatures);
-				project.setDescription(description, null);
-				return;
+			for (int i = 0; i < natures.length; ++i) {
+				if (SampleNature.NATURE_ID.equals(natures[i])) {
+					// Remove the nature
+					String[] newNatures = new String[natures.length - 1];
+					System.arraycopy(natures, 0, newNatures, 0, i);
+					System.arraycopy(natures, i + 1, newNatures, i, natures.length - i - 1);
+					description.setNatureIds(newNatures);
+					project.setDescription(description, null);
+					return;
+				}
 			}
-		}
 
-		// Add the nature
-		String[] newNatures = new String[natures.length + 1];
-		System.arraycopy(natures, 0, newNatures, 0, natures.length);
-		newNatures[natures.length] = SampleNature.NATURE_ID;
-		description.setNatureIds(newNatures);
-		project.setDescription(description, null);
-		addComponentFolderTemplate(project);
-		addApplicationXml(project);
-		updateMavenDependency(project);
+			// Add the nature
+			String[] newNatures = new String[natures.length + 1];
+			System.arraycopy(natures, 0, newNatures, 0, natures.length);
+			newNatures[natures.length] = SampleNature.NATURE_ID;
+			description.setNatureIds(newNatures);
+			project.setDescription(description, null);
+			addComponentFolderTemplate(project);
+			addApplicationXml(project);
+			updateMavenDependency(project);
+	    }
 	}
 
 	/*
---
+++ plugin/bundles/org.simplity.eclipse.plugin.validator/src/org/simplity/eclipse/plugin/validator/hyperlink/Hyperlink.java
@@ -0,0 +1,77 @@
+package org.simplity.eclipse.plugin.validator.hyperlink;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.hyperlink.IHyperlink;
+import org.eclipse.swt.program.Program;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.texteditor.IDocumentProvider;
+import org.eclipse.ui.texteditor.ITextEditor;
+import org.simplity.eclipse.plugin.validator.Validator;
+
+public class Hyperlink implements IHyperlink {
+
+	private IRegion region;
+	private IFile file;
+	private int lineNumber;
+
+	public Hyperlink(IRegion urlRegion, IFile file) {
+		this.region = urlRegion;
+		this.file = file;
+		this.lineNumber = 1;
+	}
+
+	@Override
+	public IRegion getHyperlinkRegion() {
+		return region;
+	}
+
+	@Override
+	public String getTypeLabel() {
+		return null;
+	}
+
+	@Override
+	public String getHyperlinkText() {
+		return file.getFullPath().toOSString();
+	}
+
+	@Override
+	public void open() {
+		
+		try {
+			IEditorPart editorPart = IDE.openEditor(
+					Validator.getDefault().getWorkbench().getActiveWorkbenchWindow().getActivePage(), file, false);
+			
+/*
+			Program program= Program.findProgram("xml");
+			if (program != null)
+				program.execute(file.getName());
+				
+			final ITextEditor editor = (ITextEditor) editorPart;
+
+			final IDocumentProvider provider = editor.getDocumentProvider();
+			final IDocument document = provider.getDocument(editor.getEditorInput());
+
+			final int start = document.getLineOffset(lineNumber - 1);
+			editor.selectAndReveal(start, 0);
+*/
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		/*
+		 * if (region != null) { Program.launch(fUrlString + ".xml"); return; }
+		 * //PreferencesUtil.createPreferenceDialogOn(Display.getDefault().
+		 * getActiveShell(), null, null, null).open(); Shell shell =
+		 * PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+		 * System.out.println("text value : "+shell.getData().toString());
+		 * MessageDialog.openInformation(shell, "Clicked", "Clicked on test");
+		 */
+	}
+}
